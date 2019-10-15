30f6ede60339e07d733ffee48f127b869a8097be
Santiago Alcala <santiago.alcala@wolox.com.ar>
2019-10-15 11:54:33 -0300

    Fix model

--- app/models/user.js
+++ app/models/user.js
@@ -2,11 +2,13 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'users',
     {
+      id: { type: DataTypes.INTEGER },
       first_name: { type: DataTypes.STRING },
       last_name: { type: DataTypes.STRING },
-      mail: { type: DataTypes.STRING },
-      phone: { type: DataTypes.STRING },
-      passowrd: { type: DataTypes.STRING }
+      email: { type: DataTypes.STRING },
+      password: { type: DataTypes.STRING },
+      created_at: { type: DataTypes.DATE },
+      updated_at: { type: DataTypes.DATE }
     },
     {
       timestamps: false
