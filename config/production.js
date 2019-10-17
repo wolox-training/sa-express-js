f39a2451351e47cd355cfa92d4f79dacd54b3133
Santiago Alcala <santiago.alcala@wolox.com.ar>
2019-10-17 12:39:26 -0300

    test

--- config/production.js
+++ config/production.js
@@ -2,7 +2,11 @@ exports.config = {
   environment: 'production',
   common: {
     database: {
-      name: process.env.DB_NAME
+      name: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+      host: process.env.HOST,
+      port: process.env.PORT
     }
   },
   isProduction: true
