db8d0dfde790ce8ae70a7896305e2e34f77abad9
Santiago Alcala <santiago.alcala@wolox.com.ar>
2019-10-25 15:55:07 -0300

    Fixed the migration

--- migrations/migrations/20191011170825-create_users.js
+++ migrations/migrations/20191011170825-create_users.js
@@ -23,6 +23,14 @@ module.exports = {
       password: {
         type: Sequelize.STRING,
         allowNull: false
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
       }
     });
   },
